9340ad6d334df3c90666e36c17a99b1037f56d79
Vladislav Ponomarev <farazeus@gmail.com>
2021-10-03 06:17:11 +0200

    Add create_or_update_asset interactor + spec

---
+++ app/interactors/assets/create_or_update_asset.rb
@@ -0,0 +1,72 @@
+module Assets
+  class CreateOrUpdateAsset < BaseInteractor
+    class Contract < BaseContract
+      params do
+        required(:currency).filled(included_in?: CurrencyCollection.all) # TODO: finded asset currency equals that
+        required(:add_price_in_cents).filled(:integer) # TODO: gteq 0
+        required(:add_quantity).filled(:decimal) # TODO: gt 0
+        required(:asset_symbol).filled(type?: AssetSymbol)
+        required(:broker).filled(type?: Broker)
+      end
+    end
+
+    include Dry::Transaction
+
+    validate_input contract: Contract
+    map :find_or_initialize_asset
+    map :calculate_attributes
+    step :save_asset
+
+    private
+
+    def find_or_initialize_asset(input)
+      asset = Asset.find_or_initialize_by(asset_symbol: input[:asset_symbol])
+      asset.average_price_currency = input[:currency]
+      input.merge(asset: asset)
+    end
+
+    def calculate_attributes(input)
+      input[:asset] =
+        if input[:asset].persisted?
+          calculate_attributes_for_persisted_asset(input)
+        else
+          calculate_attributes_for_new_asset(input)
+        end
+      input
+    end
+
+    def calculate_attributes_for_persisted_asset(input)
+      asset = input[:asset]
+      params = {
+        old_price: asset.average_price_in_cents,
+        old_quantity: asset.quantity,
+        add_price: input[:add_price_in_cents],
+        add_quantity: input[:add_quantity]
+      }
+      asset.average_price_in_cents = NewAveragePriceCalculation.call(params).success
+      asset.quantity = asset.quantity + input[:add_quantity]
+      asset.quantity_in_brokers[input[:broker].name] =
+        if asset.quantity_in_brokers[input[:broker].name].present?
+          asset.quantity_in_brokers[input[:broker].name].to_d + input[:add_quantity]
+        else
+          input[:add_quantity]
+        end
+      asset
+    end
+
+    def calculate_attributes_for_new_asset(input)
+      asset = input[:asset]
+      asset.average_price_in_cents = input[:add_price_in_cents]
+      asset.quantity = input[:add_quantity]
+      asset.quantity_in_brokers = Hash.new(0)
+      asset.quantity_in_brokers[input[:broker].name] = input[:add_quantity]
+      asset
+    end
+
+    def save_asset(input)
+      Success(asset: input[:asset].save!)
+    rescue => e
+      Failure(e)
+    end
+  end
+end
---
+++ spec/interactors/assets/create_or_update_asset_spec.rb
@@ -0,0 +1,57 @@
+require "rails_helper"
+
+RSpec.describe Assets::CreateOrUpdateAsset, type: :interactor do
+  let_it_be(:asset_symbol) { create :asset_symbol, :with_exchange }
+  let_it_be(:broker) { create :broker }
+  let_it_be(:another_broker) { create :broker }
+  let(:currency) { "RUB" }
+  let(:add_price_in_cents) { 123_00 }
+  let(:add_quantity) { 5 }
+  let(:params) do
+    {
+      currency: currency,
+      add_price_in_cents: add_price_in_cents,
+      add_quantity: add_quantity,
+      asset_symbol: asset_symbol,
+      broker: broker
+    }
+  end
+
+  subject { described_class.call(params) }
+
+  context "when params are valid" do
+    context "when there is no asset with same asset_symbol" do
+      it { is_expected.to be_success }
+
+      it "creates new asset" do
+        expect { subject }
+          .to change { Asset.count }.from(0).to(1)
+      end
+    end
+
+    context "when there is asset with same asset_symbol" do
+      let!(:asset) do
+        create :asset, asset_symbol: asset_symbol,
+                       quantity: 2,
+                       average_price_in_cents: 100,
+                       average_price_currency: currency,
+                       quantity_in_brokers: {
+                         "#{broker.name}": "1.0",
+                         "#{another_broker.name}": "1"
+                       }
+      end
+
+      it { is_expected.to be_success }
+
+      it "updates existing asset" do
+        expect { subject }
+          .to not_change { Asset.count }.from(1)
+          .and change { Asset.take.quantity }.from(2).to(7)
+          .and change { Asset.take.average_price_in_cents }.from(100).to(8814.285714)
+          .and not_change { Asset.take.average_price_currency }.from("RUB")
+          .and change { Asset.take.quantity_in_brokers[broker.name] }.from("1.0").to("6.0")
+          .and not_change { Asset.take.quantity_in_brokers[another_broker.name] }.from("1")
+      end
+    end
+  end
+end
