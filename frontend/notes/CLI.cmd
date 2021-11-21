aa20520f2ce065e6d29774dae05ac0d5da59edad
Vladislav Ponomarev <farazeus@gmail.com>
2021-11-21 03:56:22 +0200

    Add frontend expo generated react native app

---
+++ frontend/notes/CLI.cmd
@@ -0,0 +1,16 @@
+# CLI notes
+
+## Repository creation
+
+```sh
+npx expo-cli init lazy_assets_frontend
+cd lazy_assets_frontend
+yarn run start
+
+###
+npm install -g sharp-cli
+```
+
+## Webpack analyze
+
+<https://docs.expo.dev/guides/web-performance/>
