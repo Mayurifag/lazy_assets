c83417fee0b7620255d23e456eec64c5d8693563
Vladislav Ponomarev <farazeus@gmail.com>
2021-12-04 19:59:29 +0200

    Move testprof requires to separate files

--- backend/spec/rails_helper.rb
+++ backend/spec/rails_helper.rb
@@ -3,8 +3,6 @@ require File.expand_path("../config/environment", __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 
 require "rspec/rails"
-require "test_prof/recipes/rspec/before_all"
-require "test_prof/recipes/rspec/let_it_be"
 
 Dir[Rails.root.join("spec", "support", "**", "*.rb")].sort.each { |f| require f }
 
---
+++ backend/spec/support/test_prof.rb
@@ -0,0 +1,2 @@
+require "test_prof/recipes/rspec/before_all"
+require "test_prof/recipes/rspec/let_it_be"
