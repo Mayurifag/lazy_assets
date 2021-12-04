22de21471516b900ac7fd5148caf961660da1533
Vladislav Ponomarev <farazeus@gmail.com>
2021-12-04 02:10:44 +0200

    Add sidekiq

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  if FeaturesConfig.enable_graphiql?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql" if FeaturesConfig.enable_graphiql?
+  if FeaturesConfig.enable_sidekiq_dashboard?
+    require "sidekiq/web"
+    mount Sidekiq::Web, at: "/sidekiq"
   end
   post "/graphql", to: "graphql#execute"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
